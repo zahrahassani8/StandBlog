e8e1e4bde0d369a460d0741080bbbbb1f4ed2bc6
Zahra <zahraahmadi4589@gmail.com>
2024-03-02 16:58:31 +0330

    Implementation of login user

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+app_name = "account"
 urlpatterns = [
     path('login/', views.login_user, name="login"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        
     return render(request, 'account/login.html')
