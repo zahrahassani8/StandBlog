9b6160e47b6e5c5fcde3a0bdd53d9601ef4e081b
Zahra <zahraahmadi4589@gmail.com>
2024-03-02 17:54:18 +0330

    Implementation of register user

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth.models import User
 def login_user(request):
     if request.user.is_authenticated:
         return redirect('/')
@@ -18,3 +18,18 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('/')
+
+
+def register_user(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 == password2:
+            user = User.objects.create(username=username, email=email, password=password1)
+            user.save()
+            return redirect('/')
+    return render(request, 'account/register.html')
